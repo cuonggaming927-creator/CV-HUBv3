6f6a9fc01bc515cfe5b2fbcc6abc7d8909a4b2f7
cuonggaming927-creator <cuonggaming927@gmail.com>
2026-02-19 01:57:30 +0700

    Update cvhub.lua

--- cvhub.lua
+++ cvhub.lua
@@ -1,4 +1,4 @@
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local  WinUI = loadstring(game:HttpGet("http://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Window = WindUI:CreateWindow({
     Title = "Escape Tsunami", -- Tên hiển thị
     Icon = "rbxassetid://10734951102", -- ID icon (tùy chọn)
