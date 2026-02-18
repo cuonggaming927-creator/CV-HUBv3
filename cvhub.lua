8f2714dc8efd3f6bdce13845a97731fd921d208f
cuonggaming927-creator <cuonggaming927@gmail.com>
2026-02-19 01:15:40 +0700

    Update cvhub.lua

--- cvhub.lua
+++ cvhub.lua
@@ -1,6 +1,6 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 WindUI:AddTheme({
-    Name = "My Theme", -- theme name
+    Name = "Escape Tsunami", -- theme name
     
     Accent = Color3.fromHex("#18181b"),
     Background = Color3.fromHex("#101010"), -- Accent
@@ -10,3 +10,10 @@ WindUI:AddTheme({
     Button = Color3.fromHex("#52525b"),
     Icon = Color3.fromHex("#a1a1aa"),
 })
+WindUI:Gradient({                                                      
+    ["0"] = { Color = Color3.fromHex("#1f1f23"), Transparency = 0.5 },            
+    ["100"]   = { Color = Color3.fromHex("#18181b"), Transparency = 0.5 },      
+}, {                                                                            
+    Rotation = 0,                                                               
+}), 
+WindUI:SetTheme("Escape Tsunami")
