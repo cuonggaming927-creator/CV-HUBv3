39d3634639dada65507133933c222ca1fa1da820
cuonggaming927-creator <cuonggaming927@gmail.com>
2026-02-19 01:10:22 +0700

    Update cvhub.lua

--- cvhub.lua
+++ cvhub.lua
@@ -1,18 +1,18 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-local Window = WindUI:CreateWindow({
-    Title = "My Super Hub",
-    Icon = "door-open", -- lucide icon
-    Author = "by .ftgs and .ftgs",
-    Folder = "MySuperHub",
+WindUI:AddTheme({
+    Name = "My Theme", -- theme name
     
-    -- ↓ This all is Optional. You can remove it.
-    Size = UDim2.fromOffset(580, 460),
-    MinSize = Vector2.new(560, 350),
-    MaxSize = Vector2.new(850, 560),
-    Transparent = true,
-    Theme = "Dark",
-    Resizable = true,
-    SideBarWidth = 200,
-    BackgroundImageTransparency = 0.42,
-    HideSearchBar = true,
-    ScrollBarEnabled = false,
+    Accent = Color3.fromHex("#18181b"),
+    Background = Color3.fromHex("#101010"), -- Accent
+    Outline = Color3.fromHex("#FFFFFF"),
+    Text = Color3.fromHex("#FFFFFF"),
+    Placeholder = Color3.fromHex("#7a7a7a"),
+    Button = Color3.fromHex("#52525b"),
+    Icon = Color3.fromHex("#a1a1aa"),
+})
+WindUI:Gradient({                                                      
+    ["0"] = { Color = Color3.fromHex("#1f1f23"), Transparency = 0.5 },            
+    ["100"]   = { Color = Color3.fromHex("#18181b"), Transparency = 0.5 },      
+}, {                                                                            
+    Rotation = 0,                                                               
+}), 
