161fc9a7f1159e39cf474353a04ba5708ca40a7f
cuonggaming927-creator <cuonggaming927@gmail.com>
2026-02-19 01:25:02 +0700

    Update cvhub.lua

--- cvhub.lua
+++ cvhub.lua
@@ -1,19 +1,11 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-WindUI:AddTheme({
-    Name = "Escape Tsunami", -- theme name
-    
-    Accent = Color3.fromHex("#18181b"),
-    Background = Color3.fromHex("#101010"), -- Accent
-    Outline = Color3.fromHex("#FFFFFF"),
-    Text = Color3.fromHex("#FFFFFF"),
-    Placeholder = Color3.fromHex("#7a7a7a"),
-    Button = Color3.fromHex("#52525b"),
-    Icon = Color3.fromHex("#a1a1aa"),
+local Window = WindUI:CreateWindow({
+    Title = "Escape Tsunami", -- Tên hiển thị
+    Icon = "rbxassetid://10734951102", -- ID icon (tùy chọn)
+    Author = "By tiktok:toibitretrau11222", -- Tên tác giả
+    Folder = "MyscriptEscapeTsunami" -- Tên thư mục lưu cấu hình
 })
-WindUI:Gradient({                                                      
-    ["0"] = { Color = Color3.fromHex("#1f1f23"), Transparency = 0.5 },            
-    ["100"]   = { Color = Color3.fromHex("#18181b"), Transparency = 0.5 },      
-}, {                                                                            
-    Rotation = 0,                                                               
-}), 
-WindUI:SetTheme("Escape Tsunami")
+local MainTab = Window:CreateTab({
+    Name = "Main",
+    Icon = "rbxassetid://10734951102"
+})
